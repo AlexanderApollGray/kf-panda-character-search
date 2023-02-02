39c325351fd92ede3ac41b33ebd5ebe7bb123071
AlexanderApollGray <90724200+AlexanderApollGray@users.noreply.github.com>
2023-02-02 10:23:26 -0700

    ihu

---
+++ script.js
@@ -0,0 +1,10 @@
+// KF Panda Character Search
+
+// Get Input
+let name = prompt("Enter a name:");
+
+// Check the name
+if (name === "Tigress") {
+    alert("Yes, Tigress is a kf panda character!")
+}
+
